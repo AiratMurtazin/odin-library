169a7f85be836af79d7757516bd4c1592e7302c8
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-11-02 21:07:52 +0300

    add different styles of read unRead

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const myLibrary = []
 const modal = document.querySelector('.modalWindow')
 const openModal = document.querySelector('.new-book')
 const closeModal = document.querySelector('.closeModal')
-
-// const titleForm = document.getElementById('titleForm').value
-// const authorForm = document.getElementById('authorForm').value
-// const pagesForm = document.getElementById('pagesForm').value
-// const readForm = document.getElementById('readForm').value
-
 const submitBtn = document.querySelector('.submitForm')
 const bookForm = document.querySelector('.book-form')
 
@@ -23,26 +17,12 @@ function Book(title, author, pages, read) {
 			<h2 class="el title">${this.title}</h2>
 			<h4 class="el author">${this.author}</h4>
 			<p class="el pages">${this.pages}</p>
-			<p class="el read">${this.read ? 'Have read already' : 'Not read yet'}</p></p>
+			<p class="el ${this.read ? 'read' : 'notRead'}">${
+			this.read ? 'Read' : 'Not read'
+		}</p></p>
 		</article>`
 	}
 }
-
-const theMetro2033 = new Book(
-	'Metro 2033',
-	'Dmitri Gluhovsky',
-	321,
-	'have read already'
-)
-
-const generationP = new Book(
-	'Generation P',
-	'Viktor Pelevin',
-	260,
-	'have read already'
-)
-
-const theWorm = new Book('The Worm', 'John Faulse', 500, 'not read yet')
 
 function addBookToLibrary(book) {
 	myLibrary.push(book)
@@ -54,13 +34,11 @@ function displayEachBook() {
 
 	myLibrary.forEach(book => {
 		const bookInfo = book.info()
-		const div = document.createElement('div')
-		div.innerHTML = bookInfo
-		bookContainer.appendChild(div)
+		bookContainer.innerHTML = bookInfo
+		document.body.appendChild(bookContainer)
 	})
 }
 
-// addBookToLibrary(new Book(...['The Sun', 'Ernest Heminguay', 200, true]))
 openModal.addEventListener('click', () => {
 	modal.showModal()
 })
@@ -81,6 +59,7 @@ bookForm.addEventListener('submit', e => {
 			document.getElementById('readForm').checked
 		)
 	)
+
 	modal.close()
 	displayEachBook()
 	document.getElementById('titleForm').value = ''
@@ -88,3 +67,16 @@ bookForm.addEventListener('submit', e => {
 	document.getElementById('pagesForm').value = ''
 	document.getElementById('readForm').checked = false
 })
+
+function changeRead() {
+	console.log('kak to tak')
+	document.querySelector('.read').classList.remove('read')
+	document.querySelector('.read').classList.add('notRead')
+}
+// const haveRead = document.querySelector('.read')
+// const notHaveRead = document.querySelector('.notRead')
+// haveRead.addEventListener('click', () => {
+// 	console.log('ty nashal')
+// 	haveRead.classList.remove('read')
+// 	haveRead.classList.add('notRead')
+// })
