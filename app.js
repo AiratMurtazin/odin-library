d18baa96f3e53e52fe3ea7633b4eafb3d01bd73b
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-11-06 19:20:12 +0300

    first refactor of js

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
 'use strict'
 
+// Define an array to store the library books
 const myLibrary = []
+
+// Get DOM elements
 const modal = document.querySelector('.modalWindow')
 const openModal = document.querySelector('.new-book')
 const closeModal = document.querySelector('.closeModal')
-const submitBtn = document.querySelector('.submitForm')
 const bookForm = document.querySelector('.book-form')
+const bookContainer = document.getElementById('book-container')
+
+// Book constructor function
 function Book(title, author, pages, read) {
 	this.title = title
 	this.author = author
 	this.pages = pages
 	this.read = read
-	this.info = function () {
-		return `<article class="single-book">
-			<h2 class="el title">${this.title}</h2>
-			<h4 class="el author">${this.author}</h4>
-			<p class="el pages">${this.pages}</p>
-			<button class="el btnRead ${this.read ? 'read' : 'notRead'}">${
-			this.read ? 'Read' : 'Not read'
-		}</button>
-		<button class="removeBook">Remove</button>
-		</article>`
-	}
 }
 
+// Book prototype to generate book info HTML
+Book.prototype.info = function () {
+	return `
+    <article class="single-book">
+      <h2 class="el title">${this.title}</h2>
+      <h4 class="el author">${this.author}</h4>
+      <p class="el pages">${this.pages}</p>
+      <button class="el btnRead ${this.read ? 'read' : 'notRead'}">${
+		this.read ? 'Read' : 'Not read'
+	}</button>
+      <button class="removeBook">Remove</button>
+    </article>
+  `
+}
+
+// Function to add a book to the library
 function addBookToLibrary(book) {
 	myLibrary.push(book)
 }
 
+// Function to display all books in the library
 function displayEachBook() {
-	const bookContainer = document.getElementById('book-container')
 	bookContainer.innerHTML = ''
 
 	myLibrary.forEach((book, index) => {
@@ -37,56 +47,50 @@ function displayEachBook() {
 		const div = document.createElement('div')
 		div.innerHTML = bookInfo
 		bookContainer.appendChild(div)
+
 		const btnRead = div.querySelector('.btnRead')
 		const removeBook = div.querySelector('.removeBook')
 
-		btnRead.addEventListener('click', e => {
-			if (e.currentTarget.classList.contains('read')) {
-				e.currentTarget.classList.remove('read')
-				e.currentTarget.classList.add('notRead')
-				e.currentTarget.textContent = 'Not read'
-			} else {
-				e.currentTarget.classList.remove('notRead')
-				e.currentTarget.classList.add('read')
-				e.currentTarget.textContent = 'Read'
-			}
+		btnRead.addEventListener('click', () => {
+			// Toggle the read status
+			book.read = !book.read
+			displayEachBook()
 		})
-		removeBook.addEventListener('click', e => {
-			let indexToRemove = myLibrary.indexOf(book)
-			if (indexToRemove !== -1) {
-				myLibrary.splice(indexToRemove, 1)
-				displayEachBook()
-			}
+
+		removeBook.addEventListener('click', () => {
+			// Remove the book from the library
+			myLibrary.splice(index, 1)
+			displayEachBook()
 		})
 	})
 }
 
+// Event listener for opening the modal
 openModal.addEventListener('click', () => {
 	modal.showModal()
 })
 
-closeModal.addEventListener('click', e => {
-	e.preventDefault()
+// Event listener for closing the modal
+closeModal.addEventListener('click', () => {
 	modal.close()
 })
 
+// Event listener for submitting the book form
 bookForm.addEventListener('submit', e => {
 	e.preventDefault()
 
-	addBookToLibrary(
-		new Book(
-			document.getElementById('titleForm').value,
-			document.getElementById('authorForm').value,
-			document.getElementById('pagesForm').value,
-			document.getElementById('readForm').checked
-		)
+	// Create a new book and add it to the library
+	const newBook = new Book(
+		document.getElementById('titleForm').value,
+		document.getElementById('authorForm').value,
+		document.getElementById('pagesForm').value,
+		document.getElementById('readForm').checked
 	)
 
-	modal.close()
-	displayEachBook()
+	addBookToLibrary(newBook)
 
-	document.getElementById('titleForm').value = ''
-	document.getElementById('authorForm').value = ''
-	document.getElementById('pagesForm').value = ''
-	document.getElementById('readForm').checked = false
+	// Close the modal, reset the form, and display the updated library
+	modal.close()
+	bookForm.reset()
+	displayEachBook()
 })
